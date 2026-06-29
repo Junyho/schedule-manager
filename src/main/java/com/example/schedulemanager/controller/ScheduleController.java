01c5c4288cf2e9ad4e84d8757bad96affe755ee2
junyho <inop159@naver.com>
2026-06-29 09:51:01 +0900

    feat: 일정 CRUD 기능 구현

---
+++ src/main/java/com/example/schedulemanager/controller/ScheduleController.java
@@ -0,0 +1,47 @@
+package com.example.schedulemanager.controller;
+
+import com.example.schedulemanager.dto.*;
+import com.example.schedulemanager.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+public class ScheduleController {
+    private final ScheduleService scheduleService;
+
+    @PostMapping("/schedules")
+    public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.save(request));
+    }
+
+    @GetMapping("/schedules")
+    public ResponseEntity<List<GetScheduleResponse>> getSchedules(@RequestParam(required = false) String name) {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.findByName(name));
+    }
+
+    @GetMapping("/schedules/{scheduleId}")
+    public ResponseEntity<GetScheduleResponse> getSchedule(@PathVariable Long scheduleId) {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.findOne(scheduleId));
+    }
+
+    @PatchMapping("/schedules/{scheduleId}")
+    public ResponseEntity<UpdateScheduleResponse> update(
+            @RequestBody UpdateScheduleRequest request,
+            @PathVariable Long scheduleId) {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.update(request, scheduleId));
+    }
+
+    @DeleteMapping("/schedules/{scheduleId}")
+    public ResponseEntity<Void> delete(
+            @RequestBody DeleteScheduleRequest request,
+            @PathVariable Long scheduleId) {
+        scheduleService.delete(request, scheduleId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
---
+++ src/main/java/com/example/schedulemanager/service/ScheduleService.java
@@ -0,0 +1,122 @@
+package com.example.schedulemanager.service;
+
+import com.example.schedulemanager.dto.*;
+import com.example.schedulemanager.entity.Schedule;
+import com.example.schedulemanager.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+
+    @Transactional
+    public CreateScheduleResponse save(CreateScheduleRequest request) {
+        Schedule schedule = new Schedule(
+                request.getTitle(),
+                request.getContent(),
+                request.getName(),
+                request.getPassword()
+        );
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+        return new CreateScheduleResponse(
+                savedSchedule.getId(),
+                savedSchedule.getTitle(),
+                savedSchedule.getContent(),
+                savedSchedule.getName(),
+                savedSchedule.getCreatedAt(),
+                savedSchedule.getModifiedAt()
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public List<GetScheduleResponse> findByName(String name) {
+        List<GetScheduleResponse> dtos = new ArrayList<>();
+
+        if (name == null || name.isBlank()) {
+            List<Schedule> schedules = scheduleRepository.findAll();
+            for (Schedule schedule : schedules) {
+                GetScheduleResponse dto = new GetScheduleResponse(
+                        schedule.getId(),
+                        schedule.getTitle(),
+                        schedule.getContent(),
+                        schedule.getName(),
+                        schedule.getCreatedAt(),
+                        schedule.getModifiedAt()
+                );
+                dtos.add(dto);
+            }
+            return dtos;
+        }
+
+        List<Schedule> schedules = scheduleRepository.findByName(name);
+        for (Schedule schedule : schedules) {
+            GetScheduleResponse dto = new GetScheduleResponse(
+                    schedule.getId(),
+                    schedule.getTitle(),
+                    schedule.getContent(),
+                    schedule.getName(),
+                    schedule.getCreatedAt(),
+                    schedule.getModifiedAt()
+            );
+            dtos.add(dto);
+        }
+        return dtos;
+
+    }
+
+
+    @Transactional
+    public GetScheduleResponse findOne(Long scheduleId) {
+        Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(
+                () -> new IllegalStateException("없는 일정입니다.")
+        );
+
+        return new GetScheduleResponse(
+                schedule.getId(),
+                schedule.getTitle(),
+                schedule.getContent(),
+                schedule.getName(),
+                schedule.getCreatedAt(),
+                schedule.getModifiedAt()
+        );
+    }
+
+    @Transactional
+    public UpdateScheduleResponse update(UpdateScheduleRequest request, Long scheduleId) {
+        Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(
+                () -> new IllegalStateException("없는 일정입니다.")
+        );
+
+        if (!request.getPassword().equals(schedule.getPassword())) {
+            throw new IllegalStateException("비밀번호가 틀립니다.");
+        }
+        schedule.update(request.getTitle(), request.getName());
+        return new UpdateScheduleResponse(
+                schedule.getId(),
+                schedule.getTitle(),
+                schedule.getContent(),
+                schedule.getName(),
+                schedule.getCreatedAt(),
+                schedule.getModifiedAt()
+        );
+    }
+
+    @Transactional
+    public void delete(DeleteScheduleRequest request, Long scheduleId) {
+        Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(
+                () -> new IllegalStateException("없는 일정입니다.")
+        );
+
+        if (!request.getPassword().equals(schedule.getPassword())) {
+            throw new IllegalStateException("비밀번호가 틀립니다.");
+        }
+
+        scheduleRepository.deleteById(scheduleId);
+    }
+}
