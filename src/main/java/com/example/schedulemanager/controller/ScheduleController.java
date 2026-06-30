4b500cde1a640769e2ade4529dbe5452fe4b6c84
junyho <inop159@naver.com>
2026-06-30 17:56:07 +0900

    feat: 요청 DTO 검증 기능 추가

--- src/main/java/com/example/schedulemanager/controller/ScheduleController.java
+++ src/main/java/com/example/schedulemanager/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package com.example.schedulemanager.controller;
 
 import com.example.schedulemanager.dto.*;
 import com.example.schedulemanager.service.ScheduleService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,22 +17,22 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @PostMapping("/schedules")
-    public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
+    public ResponseEntity<ScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.save(request));
     }
 
     @GetMapping("/schedules")
-    public ResponseEntity<List<GetScheduleResponse>> getSchedules(@RequestParam(required = false) String name) {
+    public ResponseEntity<List<ScheduleResponse>> getSchedules(@RequestParam(required = false) String name) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.findByName(name));
     }
 
     @GetMapping("/schedules/{scheduleId}")
-    public ResponseEntity<GetScheduleResponse> getSchedule(@PathVariable Long scheduleId) {
+    public ResponseEntity<ScheduleResponse> getSchedule(@PathVariable Long scheduleId) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.findOne(scheduleId));
     }
 
     @PatchMapping("/schedules/{scheduleId}")
-    public ResponseEntity<UpdateScheduleResponse> update(
+    public ResponseEntity<ScheduleResponse> update(
             @RequestBody UpdateScheduleRequest request,
             @PathVariable Long scheduleId) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.update(request, scheduleId));
@@ -39,7 +40,7 @@ public class ScheduleController {
 
     @DeleteMapping("/schedules/{scheduleId}")
     public ResponseEntity<Void> delete(
-            @RequestBody DeleteScheduleRequest request,
+            @Valid @RequestBody DeleteScheduleRequest request,
             @PathVariable Long scheduleId) {
         scheduleService.delete(request, scheduleId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
